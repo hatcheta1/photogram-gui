3226cdc8a17528dfeaea21bcbddd496fc915212a
Ashanti Hatchett <ahatchett02@gmail.com>
2024-09-30 17:26:48 +0000

    added navbar and users#index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+    @list_of_users = User.all.order({ :username => :asc })
+
+    render({ :template => "user_templates/index" })
+  end
+
+  def show
+    url_username = params.fetch("username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.first
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show" })
+    end
+  end
+  
+end
